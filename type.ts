08ff8102f8dd81c510c252ed6c98a9e91afb04d6
Maxim Nazarov <124855838+RLafleet@users.noreply.github.com>
2023-09-22 10:12:19 +0300

    Add files via upload
    For 2 lab

---
+++ type.ts
@@ -0,0 +1,104 @@
+type Char = {
+    type: "text";
+    value: string; 
+    fontFamily: string;
+    fontSize: number; 
+    color: string; 
+    bold: boolean;
+};
+
+type TextBlock = Block & {
+    type: "text";
+    ID: number;
+    color: string;
+    chars: Char[];
+};
+
+type ImageBlock = Block & {
+    type: "image";
+    ID: number;
+    src: string;
+    opacity: number;
+
+};
+
+type squareFigure = {
+    typeOfFigure: string;
+} 
+
+type triangleFigure = {
+    typeOfFigure: string;
+} 
+type circleFigure = {
+    typeOfFigure: string;    
+} 
+
+type Figure = {
+    square: squareFigure;
+    triangle: triangleFigure;
+    circle: circleFigure; 
+}
+
+type GraphicObject = Block & {
+    ID: number;
+    color: string;
+    figures: Array<Figure>;
+    data: Object;
+};
+
+type Block = {
+    blockType: TextBlock | ImageBlock | GraphicObject;
+    colorBorder: string;
+    coordinatesX: number;
+    coordinatesY: number;
+    size: number;
+};
+
+type BackgroundBase64 = {
+    backgroundBase64: string;
+}
+
+type BackgroundUrl = {
+    BackgroundUrl: string;
+}
+
+type BackgroundColor = {
+    BackgroundColor: string;
+}
+
+type Slide = {
+    ID: number;
+    background: BackgroundBase64| BackgroundUrl | BackgroundColor; \ 
+    objects: Array<Block>;
+};
+
+type Doc = {
+    slides: Slide[];
+    name: string;
+};
+
+type Action = {
+    ID: number;
+    selectId: Array<Selection>; 
+    actionType: {
+        move: string;
+        resize: string;
+        delete: string;
+        add: string;
+    }
+    startPoint: string; // undo
+    endPoint: string; // redo
+}
+
+type SelectionSlide = {
+    selectSlide: number;
+    selectObject: Array<Number>;
+};
+
+type Editor = {
+    history: Array<Action>
+    selection: Array<SelectionSlide>;
+    doc: Doc;
+};
+
+export { Editor };
