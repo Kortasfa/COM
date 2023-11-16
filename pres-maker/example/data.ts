e0e74cc95a6b87c71cee32af6f031b5caf03e4ff
korta <kortaesaroz@gmail.com>
2023-11-16 13:58:54 +0300

    korta упростил проект, убрал лишнее

---
+++ pres-maker/example/data.ts
@@ -0,0 +1,59 @@
+import { NotesModel } from 'pres-maker/example/Notes'
+
+const notesModel1: NotesModel = {
+  viewMode: 'grid',
+  notes: [],
+}
+
+const notesModel2: NotesModel = {
+  viewMode: 'list',
+  notes: [],
+}
+
+const note1: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
+
+const note2: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
+
+const note3: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
+
+const note4: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
+
+const note5: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
+
+const note6: Note = {
+  title: 'Title1',
+  text: 'Text1',
+  background: 'f1f12f',
+  isPinned: false,
+  isArchived: false,
+}
