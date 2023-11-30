480645be653d90d887288285f24a79dc70489a5f
Ilsaf221 <124866755+Ilsaf221@users.noreply.github.com>
2023-12-01 01:27:24 +0300

    Delete src/Components/Editor/InputMethodsButton.tsx

--- src/Components/Editor/InputMethodsButton.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react'
-import { ImageBlock } from '../Block/ImageBlock/ImageBlock'
-import { Slide } from '../Slide/Slide'
-import topPanelStyles from '../Top-panel.module.css'
-
-const InputMethodsButton = () => {
-  return <div className={topPanelStyles.inputMethodsButton}></div>
-}
-export default InputMethodsButton
